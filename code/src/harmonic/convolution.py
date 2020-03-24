0024be000bf94f455d1cbb9ca0118d5215545e90
marcelroed <marcelroed@gmail.com>
2020-03-24 10:04:02 +0100

    Finalize Conv2DH, still buggy

--- code/src/harmonic/convolution.py
+++ code/src/harmonic/convolution.py
@@ -1,5 +1,5 @@
 from itertools import product
-from typing import Optional
+from typing import Optional, List
 from collections import defaultdict as dd
 
 import tensorflow as tf
@@ -14,17 +14,22 @@ class Conv2DH(keras.layers.Layer):
         super(Conv2DH, self).__init__(**kwargs)
 
         self.k_size = k_size
-        self.output_orders = out_orders
+        self.out_orders = out_orders
         self.out_channels = out_channels
         self.fourier_depth = fourier_depth
         self.fourier_weights: Optional[tf.Variable] = None
         self.in_orders, self.in_channels = None, None
+        self.delta_ms: Optional[List] = None
 
     def build(self, input_shape):
-        width, height, self.in_channels, self.in_orders = input_shape
+        batch_size, width, height, self.in_channels, self.in_orders = input_shape
+
+        self.delta_ms = list(set([out_stream - in_stream
+                             for in_stream, out_stream in product(range(self.in_orders), range(self.out_orders))]))
+
         # Initialize weights
         weight_shape = (
-            self.k_size, self.k_size, self.in_channels, self.out_channels, self.in_orders, self.out_orders
+            self.in_channels, self.out_channels, self.fourier_depth,
         )
 
         self.fourier_weights = self.add_weight(name='fourier_weights',
@@ -40,6 +45,10 @@ class Conv2DH(keras.layers.Layer):
         :param kwargs: Additional parameters (not used)
         :return:
         """
+
+
+        print(f'{self.delta_ms=}')
+
         filters = self.generate_filters()
 
         stream_lists = dd(list)
@@ -48,8 +57,8 @@ class Conv2DH(keras.layers.Layer):
             stream_lists[out_stream].append(
                 tf.nn.conv2d(x[..., in_stream, newaxis], filters[delta_m], strides=(1, 1), padding='same'))
 
-        streams = [tf.concat(l, axis=3) for l in stream_lists]
-        convolved = tf.concat(streams, axis=4)
+        streams = {out: tf.concat(l, axis=3) for out, l in stream_lists.items()}
+        convolved = tf.concat([streams[k] for k in sorted(streams.keys())], axis=4)
 
         return convolved
 
@@ -57,18 +66,14 @@ class Conv2DH(keras.layers.Layer):
         # Previous sizes
         height, width, in_channels, in_orders = input_shape
 
-        return height, width, self.out_channels, len(self.out_orders)
+        return height, width, self.out_channels, self.out_orders
 
     def generate_filters(self):
+        delta_m_tensor = tf.constant(self.delta_ms, dtype=tf.float32)
+        filter_tensor = self.filter_from_weights(self.fourier_weights, delta_m_tensor)
+
         # Dictionary for filters from input order to output order
-        filter_dict = {}
-
-        for in_m in range(self.in_orders):
-            in_dict = {}
-            for out_m in range(self.output_orders):
-                in_dict[out_m] = self.fourier_weights
-
-            filter_dict[in_m] = in_dict
+        filter_dict = {delta_m: filter_tensor[..., i] for i, delta_m in enumerate(self.delta_ms)}
 
         return filter_dict
 
@@ -76,20 +81,28 @@ class Conv2DH(keras.layers.Layer):
         """Make filters from tensor of weights corresponding to rotation orders m. The final value in each weight row
         represents the rotational offset β.
 
-        :param weights: tf.Tensor containing [m_order, N_weights + 1] trainable weights
+        :param weights: tf.Tensor containing [m_order, in_channels, out_channels, N_weights + 1] trainable weights
         :param ms: tf.Tensor containing [m_order] orders to use
+
+        :return: tf.Tensor of type c64 and shape (width, height)
         """
+        print(f'{weights.shape=}, {ms.shape=}')
         # Weights have shape [channels, fourier_depth]
-        ns = tf.constant(alternating_integers(weights.shape[1] - 1), dtype=c64)[newaxis, newaxis, :]
+        # ns for Fourier expansion
+        ns = tf.constant(alternating_integers(weights.shape[-1] - 1), dtype=tf.float32)[newaxis, newaxis, :]
+
+        print(f'{ns.shape=}')
 
         linspace = tf.cast(tf.linspace(- self.k_size / 2, self.k_size / 2, self.k_size), c64)
-        filter_grid = 1.j * linspace[:, newaxis] + linspace[:, newaxis]
+        filter_grid = 1.j * tf.reshape(linspace, (1, -1)) + tf.reshape(linspace, (-1, 1))
+        filter_grid = tf.reshape(filter_grid, (self.k_size, self.k_size, ))
 
-        radii = tf.abs(filter_grid)
-        angles = tf.cast(tf.atan2(filter_grid.imag, filter_grid.real), dtype=c64)
+        radii = tf.abs(filter_grid.resize)
+        angles = tf.atan2(tf.math.imag(filter_grid), tf.math.real(filter_grid))
 
-        exponentials = weights[:, :-1] * tf.exp(1.j * ns * radii)
-        filter_values = tf.reduce_sum(exponentials, axis=2) * tf.exp(1.j * m)
+        exponentials = tf.cast(weights[..., :-1], c64) * tf.exp(1.j * tf.cast(ns * radii, c64))
+        filter_values = tf.reduce_sum(exponentials, axis=2) * tf.exp(1.j * tf.cast(ms * angles + weights[:, -1], c64))
+        return filter_values
 
 
 def alternating_integers(n):
@@ -100,4 +113,5 @@ def alternating_integers(n):
 
 
 if __name__ == '__main__':
-    print(alternating_integers(8))
+    conv = Conv2DH()
+    conv.filter_from_weights()
