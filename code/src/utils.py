842cd9a3e34be11d39a0621b2c9118b4cbecac7e
marcelroed <marcelroed@gmail.com>
2020-04-11 21:24:40 +0200

    Complex convolution implementation

--- code/src/utils.py
+++ code/src/utils.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow import complex64 as c64
 
 re = tf.math.real
 im = tf.math.imag
@@ -8,7 +9,7 @@ def hot_shape(hot, length):
     return tuple(-1 if i == hot else 1 for i in range(length))
 
 
-def alternating_integers(n):
+def alt_range(n):
     """
     Get a sequence of integers increasing in absolute value
     :param n: How many integers to get
@@ -18,4 +19,11 @@ def alternating_integers(n):
     for i in range(n):
         integers[i] *= ((i + 1) // 2) if i % 2 else -((i + 1) // 2)
     return integers
+
+
+def complex_conv2d(x, filters, **kwargs):
+    # print(f'{x.shape=}, {filters.shape=}')
+    conv = lambda v, f: tf.nn.conv2d(v, f, **kwargs)
+    return tf.cast(conv(re(x), re(filters)) - conv(im(x), im(filters)), c64) \
+           + 1.j * tf.cast(conv(re(x), im(filters)) + conv(im(x), re(filters)), c64)
 
