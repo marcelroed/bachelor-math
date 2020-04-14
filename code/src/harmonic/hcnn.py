a92bb1df8c31be262d0ce653d7d674f155f8c85f
marcelroed <marcelroed@gmail.com>
2020-04-14 02:18:30 +0200

    Functioning HyperModel with tunable parameters

--- code/src/harmonic/hcnn.py
+++ code/src/harmonic/hcnn.py
@@ -1,3 +1,5 @@
+import datetime
+
 import tensorflow.keras as keras
 import tensorflow as tf
 from data.mnist import get_mnist
@@ -5,6 +7,7 @@ from data.mnist import get_mnist
 from kerastuner import HyperModel, HyperParameters, RandomSearch
 
 from harmonic.convolution import Conv2DH
+from harmonic.functions import AvgPool2DH, HNonLinearity, HBatchNormalization, HFlatten
 
 
 class HCNN(HyperModel):
@@ -14,20 +17,38 @@ class HCNN(HyperModel):
     def build(self, hp: HyperParameters):
         model = keras.models.Sequential([
             keras.layers.Reshape((28, 28, 1, 1)),  # Introduce streams
-            keras.layers.Lambda(lambda v: tf.stack((v, tf.zeros_like(v)), axis=-1)),
+            keras.layers.Lambda(lambda v: tf.stack((v, tf.zeros_like(v)), axis=-1)),  # Imaginary part initialized to 0
             keras.layers.Lambda(print_return),
-            Conv2DH(out_orders=2, out_channels=8),
-            keras.layers.Lambda(print_return),
-            keras.layers.AvgPool2D(),
 
-            Conv2DH(out_orders=1, out_channels=10),
-            keras.layers.Lambda(lambda v: tf.math.reduce_euclidean_norm(v, axis=-1)),
+            # Block 1: Shape [batch, 28, 28, channels=8, streams=2, 2]
+            Conv2DH(out_orders=2, out_channels=8),
+            HNonLinearity(),  # Defaults to ReLU
+            Conv2DH(out_orders=2, out_channels=8),
+            HBatchNormalization(),
+
+            # Block 2: Shape [batch, 14, 14, channels=16, streams=2, 2]
+            AvgPool2DH(strides=(2, 2)),
+            Conv2DH(out_orders=2, out_channels=16),
+            HNonLinearity(),
+            Conv2DH(out_orders=2, out_channels=16),
+            HBatchNormalization(),
+
+            # Block 3: Shape [batch, 7, 7, channels=35, streams=2, 2]
+            AvgPool2DH(),
+            Conv2DH(out_orders=2, out_channels=35),
+            HNonLinearity(),
+            Conv2DH(out_orders=2, out_channels=35),
+
+            # Block 4: Reduce to magnitudes and apply final activation
+            HFlatten(),
             keras.layers.Lambda(print_return),
+            keras.layers.Dense(10),
             keras.layers.Softmax(),
         ])
 
         model.compile(
-            optimizer='adam',
+            optimizer=keras.optimizers.Adam(
+                learning_rate=10 ** hp.Float('log_learning_rate', -6, -1, step=0.5, default=-3)),
             loss='categorical_crossentropy',
             metrics=['accuracy']
         )
@@ -41,28 +62,21 @@ def print_return(v):
 
 
 if __name__ == '__main__':
+    # tf.keras.backend.set_floatx('float16')
+
+    dt = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     hypermodel = HCNN()
     train, test = get_mnist()
     tuner = RandomSearch(
         hypermodel,
         objective='accuracy',
-        max_trials=10,
+        max_trials=40,
         directory='models',
-        project_name='H_mnist'
+        project_name='H-MNIST-' + dt
     )
 
     print('Created tuner')
-    tuner.search(*train, epochs=5, validation_data=test)
-
-"""
-        model = keras.Sequential([
-            layer for group in [
-                [
-                    Conv2DH(5, 3),
-
-                ] for i in range(4)
-            ] for layer in group
-        ] + [
-
-        ])
-"""
+    log_dir = "logs/fit/" + dt
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    tuner.search(*train, epochs=30, validation_data=test, batch_size=32,
+                 callbacks=[tensorboard_callback])
